302a748d7b5dadc47aa5448d61ab32c63f841d34
kev1nbam27 <kevin1bam@web.de>
2024-01-04 22:38:09 +0100

    remove sanitize string from hyprland/submap

--- src/modules/hyprland/submap.cpp
+++ src/modules/hyprland/submap.cpp
@@ -51,8 +51,7 @@ void Submap::onEvent(const std::string& ev) {
     return;
   }
 
-  auto submapName = ev.substr(ev.find_last_of('>') + 1);
-  submapName = waybar::util::sanitize_string(submapName);
+  auto submapName = ev.substr(ev.find_first_of('>') + 2);
 
   submap_ = submapName;
 
